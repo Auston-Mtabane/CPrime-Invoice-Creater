5019e24c8e5b8e86f42bea786f81ba033d99ba50
Auston Mtabane <mtabane.auston@gmail.com>
2025-08-08 13:58:55 +0200

    form update

--- src/components/FormSectionInvoice.tsx
+++ src/components/FormSectionInvoice.tsx
@@ -2,16 +2,31 @@
 function FormSectionInvoice() {
   return (
     <>
-    <p>Client Invoice/Quote</p>
+    <p>Client Details</p>
     <form action="#" method="post">
-        <label htmlFor="fname" >FullName:</label>
-        <input type="name" placeholder="John Doe" id="fname" name="fname"/>
+        <div className="rounded-div">
+            <label htmlFor="fname" >FullName:</label>
+            <input type="name" placeholder="John Doe" id="fname" name="fname"/>
 
-        <label htmlFor="femail" >Email:</label>
-        <input type="email" placeholder="doejohn3@gmail.com" id="femail" name="femail"/><br />
+            <label htmlFor="femail" >Email:</label>
+            <input type="email" placeholder="doejohn3@gmail.com" id="femail" name="femail"/><br />
 
-        <label htmlFor="fphone">Mobile/Tel:</label>
-        <input type="tel" placeholder="27 61 961 0499" id="fphone" name="fphone"/>
+            <label htmlFor="fphone">Mobile/Tel:</label>
+            <input type="tel" placeholder="27 61 961 0499" id="fphone" name="fphone"/>
+        </div>
+        <p>Invoice/Quote Items</p>
+        <div className="rounded-div">
+            <div>
+                <p>Item Name/Description   Quantity   Amount (R)  Sub Total</p>
+                
+                <br />
+                <input type="email" placeholder="doejohn3@gmail.com" id="femail" name="femail"/>
+                <input type="tel" placeholder="27 61 961 0499" id="fphone" name="fphone"/>
+                <input type="tel" placeholder="27 61 961 0499" id="fphone" name="fphone"/>
+                <input type="tel" placeholder="27 61 961 0499" id="fphone" name="fphone"/>
+            </div>
+            <button >Add Item (+)</button>
+        </div>
 
         <br /><input type="submit" value="preview"/>
     </form>
