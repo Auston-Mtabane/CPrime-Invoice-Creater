257a8fe92017a125b50415e0d5e33cccc1b8ecd8
Auston Mtabane <mtabane.auston@gmail.com>
2025-08-09 19:48:19 +0200

    added: add  item

--- src/components/FormSectionInvoice.tsx
+++ src/components/FormSectionInvoice.tsx
@@ -1,45 +1,136 @@
+import React, { useState } from "react";
+import Item from "./Item";
+
+type Item = {
+  index: number
+  name: string;
+  quantity: number;
+  amount: number;
+  subtotal: number;
+}
+
+let items: Item[] = [];
 
 function FormSectionInvoice() {
+
+
+  const [item, setItem] = useState({
+    name: "",
+    quantity: "",
+    amount: "",
+  });
+
+  const addItem = () => {
+    const newItem: Item = {
+      index: items.length,
+      name: item.name,
+      quantity: Number(item.quantity),
+      amount: Number(item.amount),
+      subtotal: Number(item.quantity) * Number(item.amount),
+    };
+    items.push(newItem);
+    setItem({ name: "", quantity: "", amount: "" });
+    console.log(items);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setItem((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const subtotal = Number(item.quantity) * Number(item.amount);
+
   return (
     <>
-    <p>Client Details</p>
-    <form action="#" method="post">
+      <p>Client Details</p>
+      <form action="/" method="post">
         <div className="rounded-div" id="client-details">
-            <label htmlFor="fname" >FullName:</label>
-            <input type="name" placeholder="John Doe" id="fname" name="fname"/>
+          <label htmlFor="fname">FullName:</label>
+          <input type="name" placeholder="John Doe" id="fname" name="fname" />
 
-            <label htmlFor="femail" >Email:</label>
-            <input type="email" placeholder="doejohn3@gmail.com" id="femail" name="femail"/><br />
+          <label htmlFor="femail">Email:</label>
+          <input
+            type="email"
+            placeholder="doejohn3@gmail.com"
+            id="femail"
+            name="femail"
+          />
+          <br />
 
-            <label htmlFor="fphone">Mobile/Tel:</label>
-            <input type="tel" placeholder="27 61 961 0499" id="fphone" name="fphone"/>
+          <label htmlFor="fphone">Mobile/Tel:</label>
+          <input
+            type="tel"
+            placeholder="27 61 961 0499"
+            id="fphone"
+            name="fphone"
+          />
         </div>
         <p>Invoice/Quote Items</p>
         <div className="rounded-div">
-            <div>
-                <div className="row-item">
-                   <p>Item Name/Description</p>
-                   <p>Quantity</p>
-                   <p>Amount (R)</p>
-                   <p>Sub Total</p>
-                </div>
-                <br />
-                <div className="row-item">
-                    <input type="text" placeholder="Item" name="item-name" />
-                    <input type="number" placeholder="0" name="item-quantity" />
-                    <input type="number" placeholder="0.00" name="item-amount" />
-                    <input type="number" placeholder="0.00" name="item-subtotal" />
-                </div>
-                
+          <div>
+            <div className="row-item">
+              <p>Item Name/Description</p>
+              <p>Quantity</p>
+              <p>Amount (R)</p>
+              <p>Sub Total</p>
             </div>
-            <button >Add Item (+)</button>
+
+            <div className="added-items">
+              {items.map((item) => (
+                <Item
+                  key={item.index}
+                  index={item.index}
+                  name={item.name}
+                  quantity={item.quantity}
+                  amount={item.amount}
+                  subtotal={item.subtotal}
+                />
+
+              ))}
+            </div>
+            <br />
+            <div className="row-item">
+              <input
+                type="text"
+                placeholder="Item"
+                name="name"
+                value={item.name}
+                onChange={handleChange}
+              />
+              <input
+                type="number"
+                placeholder="0"
+                name="quantity"
+                value={item.quantity}
+                onChange={handleChange}
+              />
+              <input
+                type="number"
+                placeholder="0.00"
+                name="amount"
+                value={item.amount}
+                onChange={handleChange}
+              />
+              <input
+                type="number"
+                placeholder="0.00"
+                name="item-subtotal"
+                value={subtotal}
+                readOnly
+              />
+            </div>
+          </div>
+          <button id="add-btn" type="button" onClick={addItem}>+</button>
         </div>
 
-        <br /><input type="submit" value="preview"/>
-    </form>
+        <br />
+        <input type="submit" value="preview" />
+      </form>
     </>
-
   );
 }
 
-export default FormSectionInvoice
+export default FormSectionInvoice;
