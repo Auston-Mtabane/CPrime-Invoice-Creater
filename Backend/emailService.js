9a14c19528eaabd1a516ebcf554fe8b63ae11032
Auston Mtabane <mtabane.auston@gmail.com>
2025-08-23 17:28:26 +0200

    nodemail

---
+++ Backend/emailService.js
@@ -0,0 +1,33 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config(); // load .env variables
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  }
+});
+
+
+
+export async function sendEmail(to, subject, htmle) {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    html: htmle,
+  };
+  console.log("e: ", htmle);
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log(`Email sent to ${to} successfully`);
+    return true;
+  } catch (err) {
+    console.error(`Error sending email to ${to}:`, err);
+    throw err;
+  }
+}
--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,29 @@
 import express from 'express';
 import cors from 'cors';
+import { sendEmail } from "./emailService.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Express server is running');
-    
+
 });
 
-app.post("/api/invoice",(req,res)=>{
-    const {client, items, htmlStr }= req.body;
+app.post("/api/invoice", async (req, res) => {
+    const { client, items, html } = req.body;
     console.log('Received invoice data:', client, items);
-    res.status(200).json({message: 'Invoice data received successfully'});
+    try {
+        await sendEmail(client.femail, "Your Invoice", html);
+        res.status(200).json({ message: "Invoice sent successfully" });
+    } catch (err) {
+        res.status(500).json({ error: "Failed to send invoice email" });
+    }
 }
 
 );
 
-app.listen(3001,()=>{
+app.listen(3001, () => {
     console.log('Server is running on port http://localhost:3001 ');
 });
