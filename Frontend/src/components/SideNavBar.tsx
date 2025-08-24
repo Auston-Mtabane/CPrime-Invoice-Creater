1806bd9b310289d0fa590f818c73015adaa2c123
Auston Mtabane <mtabane.auston@gmail.com>
2025-08-24 17:45:43 +0200

    minimul working side bar

--- Frontend/src/components/SideNavBar.tsx
+++ Frontend/src/components/SideNavBar.tsx
@@ -1,18 +1,46 @@
 import { Link } from "react-router-dom";
 
-const SideNavBar =() =>{
+const SideNavBar = () => {
   return (
     <div className="sidenav">
-      <img src="../public/cplogo.svg" alt="logo" />
+      <img src="/cplogo.svg" alt="logo" style={{ width: "50%", padding: 15 }} />
       <nav>
         <ul>
-          <li><Link to="/">D</Link></li>
-          <li><Link to="/invoice-quote">I/Q</Link></li>
-          <li><Link to="/email">E</Link></li>
-          <li><Link to="/settings">S</Link></li>
+          <li>
+            <Link to="/">
+              <div>
+                <img src="/home.png" alt="" />
+                <p>Deshboard</p>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/invoice-quote">
+              <div>
+                <img src="/file-edit.png" alt="" />
+                <p>Invoice / Quote</p>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/email">
+              <div>
+                <img src="/clip-mail.png" alt="" />
+                <p>Email</p>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/settings">
+              <div>
+                <img src="/settings.png" alt="" />
+                <p>Settings</p>
+              </div>
+            </Link>
+          </li>
         </ul>
       </nav>
     </div>
   );
-}   
+};
 export default SideNavBar;
