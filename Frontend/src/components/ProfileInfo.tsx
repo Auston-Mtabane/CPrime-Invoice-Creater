9e38b972971da9521aa5537050a776c06e3ab3b5
Auston Mtabane <mtabane.auston@gmail.com>
2025-08-25 13:07:09 +0200

    editable settings form

--- Frontend/src/components/ProfileInfo.tsx
+++ Frontend/src/components/ProfileInfo.tsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
 import companyDetails from "../data/companyDetails.json";
 
-function ProfileInfo() {
+
+type Mode = "edit" | "save";
+interface SettingsPageProps {
+  mode : Mode,
+};
+
+function ProfileInfo( {mode} : SettingsPageProps) {
+  var IsEditable = mode === "save";
+  const [name,setName] = useState(companyDetails.name);
+  const [phone,setPhone] = useState(companyDetails.phone);
+  const [email,setEmail] = useState(companyDetails.email);
+  const [website,setWebsite] = useState(companyDetails.website);
+
+  const [bankName,setBankName] = useState(companyDetails.bacnkDetails.bankName);
+  const [accountHolder,setAccountHolder] = useState(companyDetails.bacnkDetails.accountHolder);
+  const [accountNumber,setAccountNumber] = useState(companyDetails.bacnkDetails.accountNumber);
+  const [bankPhoneNumber,setPhoneNumber] = useState(companyDetails.bacnkDetails.phoneNumber);
+
+
+
   return (
     <>
     <div className="rounded-div2">
@@ -8,16 +28,17 @@ function ProfileInfo() {
         Company Details
         </h2>
         <label htmlFor="name">Name:</label>
-        <input  type ="text" value={companyDetails.name} readOnly />
+        {/* change readOnly to nothing if mode == 'save' */}
+        <input  type ="text" value={name} onChange={(e)=>setName(e.target.value)} readOnly={!IsEditable} />  
         
         <label htmlFor="name">Email:</label>
-        <input  type ="text"  value={companyDetails.email} readOnly />
+        <input  type ="text"  value={email} onChange={(e)=>setEmail(e.target.value)} readOnly={!IsEditable} />
 
         <label htmlFor="name">Phone:</label>
-        <input  type ="text"  value={companyDetails.phone} readOnly />
+        <input  type ="text"  value={phone} onChange={(e)=>setPhone(e.target.value)} readOnly={!IsEditable} />
 
         <label htmlFor="name">Website:</label>
-        <input  type ="text"  value={companyDetails.website} readOnly />
+        <input  type ="text"  value={website} onChange={(e)=>setWebsite(e.target.value)} readOnly={!IsEditable} />
     </div>
 
     <div className="rounded-div2">
@@ -25,16 +46,16 @@ function ProfileInfo() {
         Bank Details
         </h2>
         <label htmlFor="name">Bank Name:</label>
-        <input  type ="text"  value={companyDetails.bacnkDetails.bankName} readOnly />
+        <input  type ="text"  value={bankName} onChange={(e)=>setBankName(e.target.value)} readOnly={!IsEditable} />
 
         <label htmlFor="name">Account Holder:</label>
-        <input  type ="text"  value={companyDetails.bacnkDetails.accountHolder} readOnly />
+        <input  type ="text"  value={accountHolder} onChange={(e)=>setAccountHolder(e.target.value)} readOnly={!IsEditable} />
 
         <label htmlFor="name">Account Number:</label>
-        <input  type ="text"  value={companyDetails.bacnkDetails.accountNumber} readOnly />
+        <input  type ="text"  value={accountNumber} onChange={(e)=>setAccountNumber(e.target.value)} readOnly={!IsEditable} />
 
         <label htmlFor="name">Phone:</label>
-        <input  type ="text"  value={companyDetails.bacnkDetails.phoneNumber} readOnly />
+        <input  type ="text"  value={bankPhoneNumber} onChange={(e)=>setPhoneNumber(e.target.value)} readOnly={!IsEditable} />
     </div>
     
 
--- Frontend/src/pages/Settings.tsx
+++ Frontend/src/pages/Settings.tsx
@@ -1,13 +1,21 @@
 import ProfileInfo from "../components/ProfileInfo";
+import { useState } from "react";
+
+type Mode = "edit" | "save";
 
 const Settings = () => {
-return (
-        <div className="page">
-            <ProfileInfo />
-            <button>
-                Edit Profile
-            </button>
-        </div>
-    );
-}
+  const [mode, setMode] = useState<Mode>("save");
+
+  const changeMode = () => {
+    setMode(mode == "save" ? "edit":"save");
+  };
+
+  return (
+    <div className="page">
+      <ProfileInfo mode={mode}/>
+      <button onClick={changeMode}>{mode}</button>
+    </div>
+  );
+};
+
 export default Settings;
